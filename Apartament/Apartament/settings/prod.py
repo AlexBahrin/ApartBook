5dc667b6ea09b4528e927132eafcb0d58c061bbd
Alexandru Bahrin <alex@Alexandrus-MacBook-Air.local>
2026-01-06 13:30:36 +0200

    changed to gmail

--- Apartament/Apartament/settings/prod.py
+++ Apartament/Apartament/settings/prod.py
@@ -34,9 +34,9 @@ DATABASES = {
 }
 
 
-# Email - Outlook SMTP
+# Email - Gmail SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
